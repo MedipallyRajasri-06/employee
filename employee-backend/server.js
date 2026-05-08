7739fb3547f2d47ff8ef2faba2ae29135af98f92
MedipallyRajasri-06 <24eg112d25@anurag.edu.in>
2026-05-08 14:04:21 +0530

    updated

--- employee-backend/server.js
+++ employee-backend/server.js
@@ -2,12 +2,15 @@ import exp from "express";
 import { connect } from "mongoose";
 import { empRoute } from "./API/empApp.js";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = exp();
 //add cors middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(",") : ["http://localhost:5173"],
   }),
 );
 //body parser middleware
@@ -18,9 +21,10 @@ app.use("/emp-api", empRoute);
 //DB connection
 const connectDB = async () => {
   try {
-    await connect("mongodb://localhost:27017/empDB");
+    await connect(process.env.MONGO_URI);
     console.log("DB connected");
-    app.listen(4000, () => console.log("server listening on port 4000.."));
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => console.log(`server listening on port ${port}..`));
   } catch (err) {
     console.log("err in DB connection", err.message);
   }
